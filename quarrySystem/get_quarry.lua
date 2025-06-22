6bfc24c0b58eaa5d1d4e3d961aef499826d3f3d3
Vital1kS <56350325+Vital1kS@users.noreply.github.com>
2025-06-22 20:28:44 +0600

    Add files via upload

--- quarrySystem/get_quarry.lua
+++ quarrySystem/get_quarry.lua
@@ -1,3 +1,5 @@
+shell.run("delete", "functions.lua")
+shell.run("delete", "miner.lua")
 shell.run("wget",
     "https://raw.githubusercontent.com/Vital1kS/ComputerCraftScripts/refs/heads/main/quarrySystem/functions.lua")
 shell.run("wget",
