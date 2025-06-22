cd30b794e9c732d91883a528a8eb336876850699
Vital1kS <56350325+Vital1kS@users.noreply.github.com>
2025-06-22 14:35:24 +0600

    Add files via upload

--- quarrySystem/functions.lua
+++ quarrySystem/functions.lua
@@ -9,18 +9,35 @@ local function checkFuel()
             local itemsInSlot = turtle.getItemCount()
             if itemsInSlot == 1 then
                 print("Add more fuel to slot 1")
-                if turtle.getFuelLevel() < 400 then
-                    printError("Fuel level is lower than safe 400 points")
-                    return false
-                end
             else
                 turtle.refuel(itemsInSlot - 1)
+            end
+            if turtle.getFuelLevel() < 500 then
+                printError("Fuel level is lower than safe 500 points")
+                return false
             end
         end
     end
     print(("Current fuel level: %d"):format(turtle.getFuelLevel()))
     return true
 end
+
+local function waitForMessage(correctMessage)
+    peripheral.find("modem", rednet.open)
+    if rednet.isOpen() then
+        print("Rednet is connected")
+        print("Waiting for message...")
+        repeat
+            sender_id, message, protocol = rednet.receive()
+            if message ~= correctMessage then
+                print("Received wrong message, continue waiting...")
+            end
+        until message == correctMessage
+        print("Received correct message")
+    end
+end
+
 return {
-    checkFuel = checkFuel
+    checkFuel = checkFuel,
+    waitForMessage = waitForMessage
 }
---
+++ quarrySystem/get_quarry.lua
@@ -0,0 +1,4 @@
+shell.run("wget",
+    "https://raw.githubusercontent.com/Vital1kS/ComputerCraftScripts/refs/heads/main/quarrySystem/functions.lua")
+shell.run("wget",
+    "https://raw.githubusercontent.com/Vital1kS/ComputerCraftScripts/refs/heads/main/quarrySystem/miner.lua")
