8c5b8e2075f658c1a094d734a5265794f5f87390
Vital1kS <56350325+Vital1kS@users.noreply.github.com>
2025-06-22 16:19:35 +0600

    Копание комнат

--- quarrySystem/miner.lua
+++ quarrySystem/miner.lua
@@ -27,7 +27,33 @@ local function mineColumn()
         turtle.up()
     end
 end
-
+local function mineDoubleRow(depth)
+    local blockMoved = 0
+    for i = 1, depth - 1, 1 do
+        turtle.dig()
+        if not turtle.forward() then
+            break
+        end
+        blockMoved = blockMoved + 1
+    end
+    for i = 1, blockMoved, 1 do
+        turtle.digDown()
+        turtle.back()
+    end
+end
+local function mineRow(depth)
+    local blockMoved = 0
+    for i = 1, depth - 1, 1 do
+        turtle.dig()
+        if not turtle.forward() then
+            break
+        end
+        blockMoved = blockMoved + 1
+    end
+    for i = 1, blockMoved, 1 do
+        turtle.back()
+    end
+end
 local function putItemsInChest(enableFilter)
     turtle.turnLeft()
     turtle.turnLeft()
@@ -53,8 +79,7 @@ local function putItemsInChest(enableFilter)
     return true
 end
 
--- Main funtion
-do
+local function mineToBedrock(startColumn, columnToMine, enableFilter)
     for i = startColumn, columnToMine, 2 do
         if not functions.checkFuel() then
             break
@@ -78,4 +103,36 @@ do
         end
     end
     putItemsInChest(enableFilter)
+end
+
+local function mineRoom(startHeight, height, depth)
+    for i = startHeight, height, 2 do
+        if not functions.checkFuel() then
+            break
+        end
+        if not putItemsInChest(enableFilter) then
+            break
+        end
+        for j = 1, i - 1, 1 do
+            if turtle.detectDown() then
+                turtle.digDown()
+            end
+            turtle.down()
+        end
+        if height - i == 0 then
+            mineRow(depth)
+        else
+            mineDoubleRow(depth)
+        end
+        for j = 1, i - 1, 1 do
+            turtle.up()
+        end
+    end
+    putItemsInChest(enableFilter)
+end
+-- Main funtion
+do
+    functions.waitForMessage("start_mining")
+    -- mineToBedrock(startColumn, columnToMine, enableFilter)
+    mineRoom(1, 3, 4)
 end
