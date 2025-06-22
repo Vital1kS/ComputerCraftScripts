6c1bbb8f1dddd64ab098ad958171b8ff3f29b4c9
Vital1kS <56350325+Vital1kS@users.noreply.github.com>
2025-06-22 14:53:17 +0600

    нечётный размер карьера, продолжение раскопок

--- quarrySystem/miner.lua
+++ quarrySystem/miner.lua
@@ -1,5 +1,9 @@
 local functions = require("functions")
 
+local columnToMine = 4
+local startColumn = 1
+local enableFilter = true
+
 local function mineDoubleColumn()
     local blockMoved = 0
     turtle.digDown()
@@ -12,43 +16,66 @@ local function mineDoubleColumn()
         turtle.up()
     end
 end
+local function mineColumn()
+    local blockMoved = 0
+    turtle.digDown()
+    while turtle.down() do
+        turtle.digDown()
+        blockMoved = blockMoved + 1
+    end
+    for i = 1, blockMoved, 1 do
+        turtle.up()
+    end
+end
 
-local function putItemsInChest()
+local function putItemsInChest(enableFilter)
     turtle.turnLeft()
     turtle.turnLeft()
     for i = 2, 16, 1 do
         turtle.select(i)
         local itemsInSlot = turtle.getItemCount()
-        if itemsInSlot > 1 then
-            if not turtle.drop(itemsInSlot - 1) then
+        local itemsToLeave = 0
+        if enableFilter then
+            itemsToLeave = 1
+        end
+        if itemsInSlot > itemsToLeave then
+            if not turtle.drop(itemsInSlot - itemsToLeave) then
                 printError("Chest's inventory is full")
                 turtle.turnRight()
                 turtle.turnRight()
                 return false
             end
         end
+
     end
     turtle.turnRight()
     turtle.turnRight()
     return true
 end
 
-local columnToMine = 16
-for i = 1, columnToMine, 2 do
-    if not functions.checkFuel() then
-        break
-    end
-    if not putItemsInChest() then
-        break
-    end
-    for j = 1, i - 1, 1 do
-        if turtle.detect() then
-            turtle.dig()
+-- Main funtion
+do
+    for i = startColumn, columnToMine, 2 do
+        if not functions.checkFuel() then
+            break
         end
-        turtle.forward()
+        if not putItemsInChest(enableFilter) then
+            break
+        end
+        for j = 1, i - 1, 1 do
+            if turtle.detect() then
+                turtle.dig()
+            end
+            turtle.forward()
+        end
+        if columnToMine - i == 0 then
+            mineColumn()
+        else
+            mineDoubleColumn()
+        end
+        for j = 1, i - 1, 1 do
+            turtle.back()
+        end
     end
-    mineDoubleColumn()
-    for j = 1, i - 1, 1 do
-        turtle.back()
-    end
+    putItemsInChest(enableFilter)
 end
